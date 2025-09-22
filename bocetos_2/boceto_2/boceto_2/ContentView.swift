05842b97b8cc2abad04ae2bf929b42b21150144f
alumno <V0034icky@hotmail.com>
2025-09-22 12:46:47 -0600

    [NUEVO] app con patrones

---
+++ bocetos_2/boceto_2/boceto_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  boceto_2
+//
+//  Created by alumno on 9/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bocetos_2/boceto_2/boceto_2/boceto_2App.swift
@@ -0,0 +1,17 @@
+//
+//  boceto_2App.swift
+//  boceto_2
+//
+//  Created by alumno on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct boceto_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
